740fd2bf120ab49fcf2175f54b392baa4bc82873
ORER <76395021+junseoksb@users.noreply.github.com>
2021-01-15 00:00:56 +0900

    Add files via upload

---
+++ STUD-main/presetscript.js
@@ -0,0 +1,17 @@
+function toggleimg1() {
+    document.getElementById("photo1").src = "Preset Lab/after1.JPG";
+}
+
+function toggleimg2() {
+    document.getElementById("photo2").src = "Preset Lab/after2.JPG";
+}
+
+function toggleimg3() {
+    document.getElementById("photo3").src = "Preset Lab/after3.JPG";
+}
+
+function toggleimg4() {
+    document.getElementById("photo4").src = "Preset Lab/after4.JPG";
+}
+
+init();
